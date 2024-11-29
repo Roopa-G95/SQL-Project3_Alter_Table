058d8ef4a6a6f9ffaab441093797c79f983ed293
Roopa-G95 <roopagokulakrishnan95@gmail.com>
2024-11-29 12:37:02 +0530

    Update Alter_Table_Data.sql

--- Alter_Table_Data.sql
+++ Alter_Table_Data.sql
@@ -3,3 +3,4 @@ Select * from Marklist;
 ALTER TABLE Marklist ADD ADDRESS varchar(20);
 ALTER TABLE Marklist ADD PARENT_NAME varchar(20);
 ALTER TABLE Marklist DROP ADDRESS;
+delete from Marklist where ID=1;
